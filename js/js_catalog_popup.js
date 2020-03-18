dccc87032e95969e0011111c3f3fec7260f2a810
regina-mart <regina-mart@mail.ru>
2020-03-18 13:31:55 +0300

    Правки

--- js/js_catalog_popup.js
+++ js/js_catalog_popup.js
@@ -1,12 +1,7 @@
-var links = document.querySelector(".goods-buy-link");
+var links = document.querySelectorAll(".goods-buy-link");
 var popup = document.querySelector(".added-to-basket");
 var close = popup.querySelector(".button-close");
 var closeTwo = popup.querySelector(".continue-shopping");
-
-links.addEventListener("click", function (evt) {
- evt.preventDefault();
- popup.classList.add("modal-show");
-}
 
 for(var i = 0; i < links.length; i++) {
  links[i].addEventListener("click", function (evt) {
